4bd6824b3bd59eefbcf76ccbf626693ab222b07a
Javier Solabre <javier.solabre@thomsonreuters.com>
2024-03-22 14:48:34 +0100

    first post

---
+++ README.md
@@ -0,0 +1,4 @@
+# General Purpose Bash Scripts
+## cylonbar
+Script to do a progress bar resembling a Cylon visor
+
---
+++ cylonbar.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# cylonbar: Cylon inspired progress bar
+SPEED=0.1
+DELCAR="\\x08"
+BARCAR="-"
+CAR="*"
+
+help(){
+    echo "progbar <pid> <bar_length>"
+    echo -en "\nexecution exaample:\nsleep 5s & cylonbar \$! 10\n"
+    exit 1
+}
+
+prepare_bar_length(){
+    for i in `seq 0 $barlength`;do
+        bar=$bar$BARCAR
+        del=$del$DELCAR
+    done
+}
+##########
+# Main
+##########
+
+# check arguments
+if [ $# -ne 2 ];then
+    help
+fi
+barlength=$(($2-1))
+pid=$1
+
+prepare_bar_length
+
+switch=0 # 0 move to right, 1 move to left
+cont=0
+#wait for $PID to stop
+while [ -d /proc/$pid ];do
+    #draw bar
+    echo -en "${bar:0:$cont}$CAR${bar:$cont+1:$barlength}" 
+
+    # increment or decrement cont to move right or left
+    [ $switch -eq 0 ] && cont=$(($cont+1)) || cont=$(($cont-1))
+
+    # if cont reach bar limits switch counter to change direction
+    # switch instruction works by doing a xor
+    [[ $cont -eq $barlength || $cont -eq 0 ]] && switch=$((switch ^ 1))
+
+    sleep $SPEED 
+
+    # delete progress bar for redrawing
+    echo -en $del
+done
+
